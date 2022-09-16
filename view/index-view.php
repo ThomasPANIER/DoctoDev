09612ca4649a675261afd9d49870a71a2db5828d
ThomasPANIER <thomaspanier.pro@gmail.com>
2022-09-16 15:36:40 +0200

    first commit

---
+++ view/index-view.php
@@ -0,0 +1,3 @@
+<?php
+include "view/template/header.php";
+include "view/template/footer.php";
---
+++ view/template/header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>DoctoDev</title>
+  <meta name="description" content="DoctoDev">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <!-- Place favicon.ico in the root directory -->
+
+  <!-- CSS only -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+        crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+<!-- Add your site or application content here -->
+<?php include "nav.php"; ?>
+<header class="bg-primary">
+  <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cupiditate deleniti deserunt distinctio
+      dolore, eveniet ipsa ipsum iure laboriosam libero molestiae quam vel voluptatem. Aspernatur enim est nisi
+      obcaecati pariatur?</span><span>At atque culpa, dolores ipsa laborum minus, obcaecati perferendis reiciendis
+      tenetur ullam ut voluptatem. Doloribus eum possimus quam unde. Accusamus deserunt dolor ipsum nisi non rerum sed
+      sequi totam ut?</span></p>
+</header>
+
+
+<p>Hello world! This is HTML5 Boilerplate.</p>
